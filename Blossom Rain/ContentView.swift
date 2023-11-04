e811148d3ef1b1beaf1f667bef9a907c3b868036
Erik Gomez <e@eriknicolasgomez.com>
2023-11-04 16:58:13 -0500

    match the website colors

--- Blossom Rain/ContentView.swift
+++ Blossom Rain/ContentView.swift
@@ -98,59 +98,79 @@ struct ContentView: View {
                         }
                     }
                 }
-                Section(header: Text("Calculated Drops")) {
+                Section(header: Text("Drops")) {
                     HStack {
-                        Text("Calcium:")
+                        Text("Calcium")
                         Spacer()
                         if unit == .milliliter {
                             Text(String(Int(round((calciumMultiplier*(0.55*calciumDropCount)*(mlVolume/4500))))))
+                                .bold()
                         } else if unit == .liter {
                             Text(String(Int(round((calciumMultiplier*(0.55*calciumDropCount)*(lVolume/4.5))))))
+                                .bold()
                         } else if unit == .gallon {
                             Text(String(Int(round((calciumMultiplier*(0.55*calciumDropCount)*(gVolume/(4500/3785)))))))
+                                .bold()
                         }
                     }
+                    .foregroundColor(.white)
+                    .listRowBackground(Color("Calcium"))
                     HStack {
-                        Text("Magnesium:")
+                        Text("Magnesium")
                         Spacer()
                         if unit == .milliliter {
                             Text(String(Int(round((magnesiumMultiplier*(0.54*magnesiumDropCount)*(mlVolume/4500))))))
+                                .bold()
                         } else if unit == .liter {
                             Text(String(Int(round((magnesiumMultiplier*(0.54*magnesiumDropCount)*(lVolume/4.5))))))
+                                .bold()
                         } else if unit == .gallon {
                             Text(String(Int(round((magnesiumMultiplier*(0.54*magnesiumDropCount)*(gVolume/(4500/3785)))))))
+                                .bold()
                         }
                     }
+                    .foregroundColor(.white)
+                    .listRowBackground(Color("Magnesium"))
                     HStack {
-                        Text("Potassium:")
+                        Text("Potassium")
                         Spacer()
                         // Compiler couldn't handle this many computations at once so splitting it into variables
                         if unit == .milliliter {
                             let potassiumMlCalculcation = potassiumMultiplier*(2*0.56*potassiumDropCount)*(mlVolume/4500)
                             Text(String(Int(round(potassiumMlCalculcation))))
+                                .bold()
                         } else if unit == .liter {
                             let potassiumLCalculcation = potassiumMultiplier*(2*0.56*potassiumDropCount)*(lVolume/4.5)
                             Text(String(Int(round(potassiumLCalculcation))))
+                                .bold()
                         } else if unit == .gallon {
                             let potassiumGCalculcation = potassiumMultiplier*(2*0.56*potassiumDropCount)*(gVolume/(4500/3785))
                             Text(String(Int(round(potassiumGCalculcation))))
+                                .bold()
                         }
                     }
+                    .foregroundColor(.white)
+                    .listRowBackground(Color("Potassium"))
                     HStack {
-                        Text("Sodium:")
+                        Text("Sodium")
                         Spacer()
                         // Compiler couldn't handle this many computations at once so splitting it into variables
                         if unit == .milliliter {
                             let sodiumMlCalculcation = potassiumMultiplier*(2*0.58*sodiumDropCount)*(mlVolume/4500)
                             Text(String(Int(round(sodiumMlCalculcation))))
+                                .bold()
                         } else if unit == .liter {
                             let sodiumLCalculcation = potassiumMultiplier*(2*0.58*sodiumDropCount)*(lVolume/4.5)
                             Text(String(Int(round(sodiumLCalculcation))))
+                                .bold()
                         } else if unit == .gallon {
                             let sodiumGCalculcation = potassiumMultiplier*(2*0.58*sodiumDropCount)*(gVolume/(4500/3785))
                             Text(String(Int(round(sodiumGCalculcation))))
+                                .bold()
                         }
                     }
+                    .foregroundColor(Color("SodiumText"))
+                    .listRowBackground(Color("Sodium"))
                 }
                 Text("Purchase these drops at [Lotus Coffee Products](https://lotuscoffeeproducts.com/collections/all)")
                     .font(.footnote)
@@ -162,37 +182,37 @@ struct ContentView: View {
             Form {
                 Section {
                     Toggle(isOn: $roundTippedDropperCalcium) {
-                        Text("Round Tipped: Calcium")
+                        Text("Calcium")
                     }
                     .onChange(of: roundTippedDropperCalcium) {
                         defaults.set(roundTippedDropperCalcium, forKey: "roundTippedDropperCalcium")
                         calculateMultipliers()
                     }
                     Toggle(isOn: $roundTippedDropperMagnesium) {
-                        Text("Round Tipped: Magnesium")
+                        Text("Magnesium")
                     }
                     .onChange(of: roundTippedDropperMagnesium) {
                         defaults.set(roundTippedDropperMagnesium, forKey: "roundTippedDropperMagnesium")
                         calculateMultipliers()
                     }
                     Toggle(isOn: $roundTippedDropperPotassium) {
-                        Text("Round Tipped: Potassium")
+                        Text("Potassium")
                     }
                     .onChange(of: roundTippedDropperPotassium) {
                         defaults.set(roundTippedDropperPotassium, forKey: "roundTippedDropperPotassium")
                         calculateMultipliers()
                     }
                     Toggle(isOn: $roundTippedDropperSodium) {
-                        Text("Round Tipped: Sodium")
+                        Text("Sodium")
                     }
                     .onChange(of: roundTippedDropperSodium) {
                         defaults.set(roundTippedDropperSodium, forKey: "roundTippedDropperSodium")
                         calculateMultipliers()
                     }
                 } header: {
-                    Text("Dropper Type")
+                    Text("Round Tipped Droppers")
                 } footer: {
-                    Text("Lotus Water ships with two types of droppers: \n**•Round Tipped**\n**•Straight Tipped**\n\nPlease configure your droppers above for accurate recipes.")
+                    Text("Lotus Water ships with two types of droppers: \n•Round Tipped\n•Straight Tipped\n\nIf your bottle has a **rounded tip**, please select it above to ensure the recipe is accurate.")
                         .font(.footnote)
                         .foregroundColor(.secondary)
                 }
