47543e594bba04ea26d74f40ca0ffa2fc85029fb
Erik Gomez <e@eriknicolasgomez.com>
2024-08-21 22:46:39 -0500

    first commit of skip

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// This is a Skip (https://skip.tools) package,
+// containing a Swift Package Manager project
+// that will use the Skip build plugin to transpile the
+// Swift Package, Sources, and Tests into an
+// Android Gradle Project with Kotlin sources and JUnit tests.
+import PackageDescription
+
+let package = Package(
+    name: "Blossom-Rain",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v17), .tvOS(.v17), .watchOS(.v10), .macCatalyst(.v17)],
+    products: [
+        .library(name: "BlossomRainApp", type: .dynamic, targets: ["BlossomRain"]),
+    ],
+    dependencies: [
+        .package(url: "https://source.skip.tools/skip.git", from: "1.0.4"),
+        .package(url: "https://source.skip.tools/skip-ui.git", from: "1.0.0")
+    ],
+    targets: [
+        .target(name: "BlossomRain", dependencies: [.product(name: "SkipUI", package: "skip-ui")], plugins: [.plugin(name: "skipstone", package: "skip")]),
+    ]
+)
